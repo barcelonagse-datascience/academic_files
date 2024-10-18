fab62efcc4c212eda694bfa854e52fa385ba507d
Andre <andre.souza@barcelonagse.eu>
2024-10-18 09:27:08 +0200

    updates

--- utils/checkSubmission.R
+++ utils/checkSubmission.R
@@ -1,9 +1,12 @@
 checkSubmission <- function(){
   file <- file.choose()
   sol.env <- new.env()
-  run <- suppressWarnings(tryCatch( source(file, local=sol.env) ,
-                   error = function(cond) message(sprintf('Your code does not seem to run. I got the following message from R: \n %s',
-                                                    conditionMessage(cond) ) )))
+  capture.output( run <- suppressWarnings(tryCatch(
+                   source(file, local=sol.env) ,
+                   error = function(cond) {
+                     message(sprintf('Your code does not seem to run. I got the following message from R: \n %s',
+                                     conditionMessage(cond) ))
+                   })) )
   envVars <- ls(envir=sol.env)
   solutionVars <- c('avgInflation', 'cnames', 'countries', 'data', 'eaMdl', 'eaPred',
             'eaVars', 'elnet', 'elnetPred','es.df', 'esCoef', 'esMdl', 'fmPred',
@@ -12,16 +15,18 @@ checkSubmission <- function(){
             'spainCols', 'T' , 'test.df', 'train.df', 'uniqCountries',
             'inflationSep24')
 
-  # Catch if not all variables are defined.
-  if ( all(solutionVars %in% envVars)){
-    cat(sprintf('Your submission seems to run and contains all required variables.'))
-  } else {
-    whichMiss <- solutionVars[ !(solutionVars %in% envVars) ]
-    cat(sprintf('I cant seem to find the following variables: \n'))
-    cat(whichMiss)
-    cat(sprintf('\n You can submit the code as is, but you will not get any points for the questions that request these variables.'))
-  }
+  # Catch if not all variables are defined, but the code runs.
+  if (length(run)>0){
+    if ( all(solutionVars %in% envVars)){
+      cat(sprintf('Your submission seems to run and contains all required variables.'))
+    } else {
+      whichMiss <- solutionVars[ !(solutionVars %in% envVars) ]
+      cat(sprintf('I cant seem to find the following variables: \n'))
+      cat(whichMiss)
+      cat(sprintf('\n You can submit the code as is, but you will not get any points for the questions that request these variables.'))
+    }
   
+  }
   
 }
 
